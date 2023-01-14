eec55480694dbd8016b910b2d32d8604df5a40ae
Likhith Raju1 <likhith.raju1@ibm.com>
2023-01-14 15:14:00 +0530

    focus using useRef

--- src/ExplainUseRef.js
+++ src/ExplainUseRef.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect, useRef } from "react";
 const ExplainUseRef = () => {
   const [name, setName] = useState("");
   const renderCount = useRef(0);
+  const inputRef = useRef();
 
   useEffect(() => {
     renderCount.current = renderCount.current + 1;
@@ -11,12 +12,14 @@ const ExplainUseRef = () => {
   return (
     <div>
       <input
+        ref={inputRef}
         type="text"
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
       <div>My name is: {name}</div>
       <div>Number of re-renders is: {renderCount?.current}</div>
+      <button onClick={() => inputRef.current.focus()}>Focus</button>
     </div>
   );
 };
