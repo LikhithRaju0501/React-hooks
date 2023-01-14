42d6f75cd0f590eff393f196ab695bb322ab50cb
Likhith Raju1 <likhith.raju1@ibm.com>
2023-01-14 15:26:48 +0530

    Previous state value using useRef

--- src/ExplainUseRef.js
+++ src/ExplainUseRef.js
@@ -4,10 +4,15 @@ const ExplainUseRef = () => {
   const [name, setName] = useState("");
   const renderCount = useRef(0);
   const inputRef = useRef();
+  const prevName = useRef('')
 
   useEffect(() => {
     renderCount.current = renderCount.current + 1;
   });
+
+  useEffect(() => {
+    prevName.current = name;
+  }, [name]);
 
   return (
     <div>
@@ -17,7 +22,7 @@ const ExplainUseRef = () => {
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
-      <div>My name is: {name}</div>
+      <div>My name is: {name} and before it was: {prevName?.current}</div>
       <div>Number of re-renders is: {renderCount?.current}</div>
       <button onClick={() => inputRef.current.focus()}>Focus</button>
     </div>
