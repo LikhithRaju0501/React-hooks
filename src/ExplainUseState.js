4706fa14811b988ac3097e86a3a20026c203db37
Likhith Raju1 <likhith.raju1@ibm.com>
2023-01-15 09:11:53 +0530

    Implemented correct way of useState

--- src/ExplainUseState.js
+++ src/ExplainUseState.js
@@ -4,9 +4,9 @@ const ExplainUseState = () => {
   const [count, setCount] = useState(0);
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount((prev) => prev + 1)}>+</button>
       <span>{count}</span>
-      <button onClick={() => setCount(count - 1)}>-</button>
+      <button onClick={() => setCount((prev) => prev - 1)}>-</button>
     </div>
   );
 };
